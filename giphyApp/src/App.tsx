eda77e07f8e81e495a845c04d2f1a8f7035547d6
Ulises Guzman <guzmangimenezulises06@gmail.com>
2024-10-07 19:56:30 -0300

    Entrega Redux

---
+++ giphyApp/src/App.tsx
@@ -0,0 +1,16 @@
+
+import { NavBar } from "./components/ui/Navbar/NavBar"
+import GiftList from "./components/GiftList/GiftList"
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <GiftList/>
+    </>
+  )
+}
+
+export default App
---
+++ giphyApp/src/components/ui/CardGift/CardGift.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { Card } from "react-bootstrap";
+
+interface IGift {
+  urlGift: string;
+  title: string;
+}
+
+interface IPropsCardGift {
+  gift: IGift;
+}
+
+export const CardGift: FC<IPropsCardGift> = ({ gift }) => {
+  return (
+      <Card style={{ width: "50%" }}>
+        <Card.Img variant="top" src={gift.urlGift} />
+        <Card.Body>
+          <Card.Title>{gift.title}</Card.Title>
+        </Card.Body>
+      </Card>
+  );
+};
+
+export default CardGift;
